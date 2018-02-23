28fd5c2518b3c964c9134d142741a3c42cac3ffd
jeppeter <jeppeter@gmail.com>
2018-02-23 09:57:55 +0800

    to make enum ok

--- test/enum/enum.go
+++ test/enum/enum.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"unsafe"
 )
 
 type CC struct {
@@ -24,37 +25,67 @@ func list_member(a interface{}) []string {
 	return names
 }
 
-func set_member(a interface{}, key string, val interface{}) error {
-	names := list_member(a)
-	founded := false
-	for _, n := range names {
-		if n == key {
-			founded = true
+func find_interface_field(a interface{}, key string) int {
+	p := reflect.ValueOf(a).Elem()
+	founded := -1
+	maxfld := p.NumField()
+	typeofT := p.Type()
+	for i := 0; i < maxfld; i++ {
+		if typeofT.Field(i).Name == key {
+			founded = i
 			break
 		}
 	}
+	return founded
+}
 
-	if !founded {
+func set_member(a interface{}, key string, val interface{}) error {
+	founded := find_interface_field(a, key)
+	if founded < 0 {
 		return fmt.Errorf("can not found [%s]", key)
 	}
 
-	s := reflect.ValueOf(a).Elem()
-	sv := s.FieldByName(key)
-	sv.Set(reflect.ValueOf(val))
+	rs := reflect.ValueOf(a).Elem()
+	rf := rs.Field(founded)
+	rf = reflect.NewAt(rf.Type(), unsafe.Pointer(rf.UnsafeAddr())).Elem()
+	rf.Set(reflect.ValueOf(val))
 	return nil
+}
+
+func is_member(a interface{}, key string) bool {
+	founed := find_interface_field(a, key)
+	if founed < 0 {
+		return false
+	}
+	return true
+}
+
+func get_value(a interface{}, key string) interface{} {
+	founded := find_interface_field(a, key)
+
+	if founded < 0 {
+		s := fmt.Sprintf("can not found [%s]", key)
+		panic(s)
+	}
+
+	rs := reflect.ValueOf(a).Elem()
+	rf := rs.Field(founded)
+	rf = reflect.NewAt(rf.Type(), unsafe.Pointer(rf.UnsafeAddr())).Elem()
+	return rf.Interface()
+
 }
 
 func main() {
 	cc := &CC{inner: "inner", Outer: "outer", inner16: uint16(16), Outer16: uint16(16)}
-	names := list_member(cc)
-	for _, n := range names {
-		fmt.Printf("%s\n", n)
-	}
 
 	set_member(cc, "Outer", "annother Outer")
 	set_member(cc, "Outer16", uint16(32))
+	set_member(cc, "inner", "annoer inner")
+	set_member(cc, "inner16", uint16(32))
 
-	fmt.Printf("inner [%s]\n", cc.Outer)
-	fmt.Printf("inner16 [%d]\n", cc.Outer16)
+	fmt.Printf("Outer [%s]\n", cc.Outer)
+	fmt.Printf("Outer16 [%d]\n", cc.Outer16)
+	fmt.Printf("inner [%s]\n", get_value(cc, "inner").(string))
+	fmt.Printf("inner16 [%d]\n", get_value(cc, "inner16").(uint16))
 	return
 }
