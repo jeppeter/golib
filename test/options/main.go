492d1ea25e953a130faea57c8477302b19d619ab
jeppeter <jeppeter@gmail.com>
2018-02-24 08:55:28 +0800

    to make opt.go ok

--- test/options/main.go
+++ test/options/main.go
@@ -1,4 +1,4 @@
-npackage main
+package main
 
 import (
 	"fmt"
--- test/options/opt.go
+++ test/options/opt.go
@@ -3,119 +3,71 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"reflect"
 	"sort"
 )
 
 type ExtArgsOptions struct {
 	logObject
-	strValue  map[string]string
-	boolValue map[string]bool
-	intValue  map[string]int
+	values map[string]interface{}
 }
 
 var opt_default_VALUE = map[string]interface{}{
-	"prog":  "",
-	"usage": "",
+	"prog":           "",
+	"usage":          "",
+	"description":    "",
+	"epilog":         "",
+	"version":        "0.0.1",
+	"errorhandler":   "exit",
+	"helphandler":    nil,
+	"longprefix":     "--",
+	"shortprefix":    "-",
+	"nohelpoption":   false,
+	"nojsonoption":   false,
+	"helplong":       "help",
+	"helpshort":      "h",
+	"jsonlong":       "json",
+	"cmdprefixadded": true,
+	"parseall":       true,
+	"screenwidth":    80,
+	"flagnochange":   false,
 }
 
 func (p *ExtArgsOptions) SetValue(k string, v interface{}) error {
-	fmt.Printf("[%s]=[%v]\n", k, v)
-	switch v.(type) {
-	case string:
-		if v != nil {
-			p.strValue[k] = fmt.Sprintf("%v", v)
-		} else {
-			p.strValue[k] = ""
-		}
-	case int:
-		p.intValue[k] = v.(int)
-	case bool:
-		p.boolValue[k] = v.(bool)
-	default:
-		p.strValue[k] = fmt.Sprintf("%v", v)
-	}
+	p.values[k] = v
 	return nil
 }
 
 func (p *ExtArgsOptions) GetValue(k string) interface{} {
 	var v interface{}
-	var k2 string
-	for k2, _ = range p.strValue {
-		if k2 == k {
-			v = p.strValue[k]
-			return v
-		}
+	var ok bool
+	v, ok = p.values[k]
+	if !ok {
+		return nil
 	}
-
-	for k2, _ = range p.intValue {
-		if k2 == k {
-			v = p.intValue[k]
-			return v
-		}
-	}
-
-	for k2, _ = range p.boolValue {
-		if k2 == k {
-			v = p.boolValue[k]
-			return v
-		}
-	}
-
-	return nil
+	return v
 }
 
 func (p *ExtArgsOptions) Format() string {
-	var skeys []string
-	var ikeys []string
-	var bkeys []string
+	var keys []string
 	var s string = ""
 	var k string
 	var cnt int = 0
 
-	skeys = make([]string, 0)
-	ikeys = make([]string, 0)
-	bkeys = make([]string, 0)
+	keys = make([]string, 0)
 
-	for k, _ = range p.strValue {
-		skeys = append(skeys, k)
+	for k, _ = range p.values {
+		keys = append(keys, k)
 	}
 
-	for k, _ = range p.intValue {
-		ikeys = append(ikeys, k)
-	}
-
-	for k, _ = range p.boolValue {
-		bkeys = append(bkeys, k)
-	}
-
-	sort.Strings(skeys)
-	sort.Strings(ikeys)
-	sort.Strings(bkeys)
+	sort.Strings(keys)
 
 	s += fmt.Sprintf("{")
 	cnt = 0
-	for _, k = range skeys {
+	for _, k = range keys {
 		if cnt > 0 {
 			s += fmt.Sprintf(";")
 		}
-		s += fmt.Sprintf("[%s]=[%s]", k, p.strValue[k])
-		cnt++
-	}
-
-	for _, k = range ikeys {
-		if cnt > 0 {
-			s += fmt.Sprintf(";")
-		}
-		s += fmt.Sprintf("[%s]=[%d]", k, p.intValue[k])
-		cnt++
-	}
-
-	for _, k = range bkeys {
-		if cnt > 0 {
-			s += fmt.Sprintf(";")
-		}
-		s += fmt.Sprintf("[%s]=[%v]", k, p.boolValue[k])
+		s += fmt.Sprintf("[%s]=[%v]", k, p.values[k])
 		cnt++
 	}
 	s += fmt.Sprintf("}")
@@ -123,39 +75,20 @@ func (p *ExtArgsOptions) Format() string {
 }
 
 func NewExtArgsOptions(s string) (p *ExtArgsOptions, err error) {
-	var v interface{}
 	var vmap map[string]interface{}
 	var k string
-	var emptymap interface{}
+	var v interface{}
 
 	p = nil
-	err = json.Unmarshal([]byte(`{}`), &emptymap)
-	if err != nil {
-		err = fmt.Errorf("%s", format_error(1, "parse [%s] error[%s]", err.Error()))
-		return
-	}
-	err = json.Unmarshal([]byte(s), &v)
+	err = json.Unmarshal([]byte(s), &vmap)
 	if err != nil {
 		err = fmt.Errorf("%s", format_error(1, "parse [%s] error[%s]", err.Error()))
 		return
 	}
 
-	p = &ExtArgsOptions{logObject: *newLogObject("extargsparse"), strValue: make(map[string]string), intValue: make(map[string]int), boolValue: make(map[string]bool)}
+	p = &ExtArgsOptions{logObject: *newLogObject("extargsparse"), values: make(map[string]interface{})}
 	for k, v = range opt_default_VALUE {
 		p.SetValue(k, v)
-	}
-
-	fmt.Printf("%s", format_error(1, "\n"))
-	switch v.(type) {
-	case map[string]interface{}:
-		vmap = v.(map[string]interface{})
-		fmt.Printf("%s", format_error(1, "\n"))
-	default:
-		fmt.Printf("%s", format_error(1, "emptymap [%s]\n", reflect.ValueOf(emptymap).Kind().String()))
-		if reflect.DeepEqual(emptymap, v) {
-			return p, nil
-		}
-		return nil, fmt.Errorf("%s", format_error(1, "type [%s] not supported for [%s]", reflect.ValueOf(v).Kind().String(), s))
 	}
 
 	for k, v = range vmap {
