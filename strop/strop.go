d8e13411b729d0978478211ab416131f5ee1b0f1
jeppeter <jeppeter@gmail.com>
2022-03-18 08:58:06 +0800

    to add strop.go

---
+++ strop/strop.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"strings"
+)
+
+func SplitLines(ins string) (retsarr []string) {
+	var sarr []string
+	var s string
+	sarr = strings.Split(ins, "\n")
+	retsarr = []string{}
+	for _, s = range sarr {
+		retsarr = append(retsarr, strings.TrimRight(s, "\r"))
+	}
+	return
+}
