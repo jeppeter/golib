3ae4efcef03a0c8ff245482ad4d1f78dd7ab0667
jeppeter <jeppeter@gmail.com>
2022-06-01 14:42:02 +0800

    to add ifindall and ifindindex in the retest.go

--- retest/retest.go
+++ retest/retest.go
@@ -47,6 +47,46 @@ func Findall_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx inte
 	return
 }
 
+func Ifindall_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx interface{}) (err error) {
+	var restr string
+	var idx int
+	var sarr []string
+	var reg *regexp.Regexp
+	var matchstrings []string
+	var j int
+	var s string
+	err = nil
+	if ns == nil {
+		return
+	}
+
+	sarr = ns.GetArray("subnargs")
+	if len(sarr) < 2 {
+		err = fmt.Errorf("findall need at least 2 args")
+		return
+	}
+	restr = sarr[0]
+	reg, err = regexp.Compile("(?i)" + restr)
+	if err != nil {
+		err = fmt.Errorf("compile [%s] error[%s]", restr, err.Error())
+		return
+	}
+
+	for idx = 1; idx < len(sarr); idx++ {
+		matchstrings = reg.FindStringSubmatch(sarr[idx])
+		if len(matchstrings) > 0 {
+			fmt.Fprintf(os.Stdout, "[%s] find all in [%s]\n", restr, sarr[idx])
+			for j, s = range matchstrings {
+				fmt.Fprintf(os.Stdout, "\t[%d] [%s]\n", j, s)
+			}
+		} else {
+			fmt.Fprintf(os.Stdout, "[%s] not find all in [%s]\n", restr, sarr[idx])
+		}
+	}
+	err = nil
+	return
+}
+
 func Findindex_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx interface{}) (err error) {
 	var sarr []string
 	var restr string
@@ -66,6 +106,44 @@ func Findindex_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx in
 
 	restr = sarr[0]
 	reg, err = regexp.Compile(restr)
+	if err != nil {
+		err = fmt.Errorf("compile [%s] error[%s]", restr, err.Error())
+		return
+	}
+
+	for idx = 1; idx < len(sarr); idx++ {
+		indexes = reg.FindStringIndex(sarr[idx])
+		if len(indexes) > 0 {
+			fmt.Fprintf(os.Stdout, "[%s] find all in [%s]\n", restr, sarr[idx])
+			fmt.Fprintf(os.Stdout, "\t[%d] [%d] [%s]\n", indexes[0], indexes[1], sarr[idx][indexes[0]:indexes[1]])
+		} else {
+			fmt.Fprintf(os.Stdout, "[%s] not find all in [%s]\n", restr, sarr[idx])
+		}
+	}
+
+	err = nil
+	return
+}
+
+func Ifindindex_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx interface{}) (err error) {
+	var sarr []string
+	var restr string
+	var reg *regexp.Regexp
+	var indexes []int
+	var idx int
+	err = nil
+	if ns == nil {
+		return
+	}
+
+	sarr = ns.GetArray("subnargs")
+	if len(sarr) < 2 {
+		err = fmt.Errorf("findindex need at least 2 args")
+		return
+	}
+
+	restr = sarr[0]
+	reg, err = regexp.Compile("(?i)" + restr)
 	if err != nil {
 		err = fmt.Errorf("compile [%s] error[%s]", restr, err.Error())
 		return
@@ -238,7 +316,9 @@ func init() {
 	Match_handler(nil, nil, nil)
 	Imatch_handler(nil, nil, nil)
 	Findall_handler(nil, nil, nil)
+	Ifindall_handler(nil, nil, nil)
 	Findindex_handler(nil, nil, nil)
+	Ifindindex_handler(nil, nil, nil)
 	Split_handler(nil, nil, nil)
 	Replace_handler(nil, nil, nil)
 }
@@ -256,6 +336,12 @@ func main() {
 			"$" : "+"
 		},
 		"findindex<Findindex_handler>## restr instr ... to find index matches##" : {
+			"$" : "+"
+		},
+		"ifindall<Ifindall_handler>## restr instr ... to find all matches in ignore case##" : {
+			"$" : "+"
+		},
+		"ifindindex<Ifindindex_handler>## restr instr ... to find index matches in ignore case##" : {
 			"$" : "+"
 		},
 		"split<Split_handler>## restr instr ... to split string with regexp##" : {
