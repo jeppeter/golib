a220b1afd01a911311b3b149c276486cf516bdfe
jeppeter <jeppeter@gmail.com>
2025-04-10 10:23:09 +0800

    to make _expand_cmds

--- src/executil/runexec_windows.go
+++ src/executil/runexec_windows.go
@@ -9,6 +9,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strconv"
 	"strings"
 	"syscall"
@@ -31,6 +32,28 @@ func _exist_file(fname string) (bval bool) {
 	return bval
 }
 
+func _expand_cmds(cmds []string) (newcmds []string) {
+	newcmds = cmds
+	if len(cmds) > 0 && runtime.GOOS == "windows" && !filepath.IsAbs(cmds[0]) {
+		var pathenv string
+		pathenv = os.Getenv("PATH")
+		if len(pathenv) > 0 {
+			var nsarr []string
+			var curdir string
+			nsarr = strings.Split(pathenv, ";")
+			for _, curdir = range nsarr {
+				var curexe string
+				curexe = fmt.Sprintf("%s\\%s", curdir, cmds[0])
+				if _exist_file(curexe) {
+					newcmds[0] = curexe
+					break
+				}
+			}
+		}
+	}
+	return
+}
+
 func RunCmdTimeout(cmds []string, timeout int) (outstr string, errstr string, exitcode int, err error) {
 	var stime time.Time
 	var etime time.Time
@@ -44,24 +67,7 @@ func RunCmdTimeout(cmds []string, timeout int) (outstr string, errstr string, ex
 	const da = syscall.STANDARD_RIGHTS_READ | syscall.PROCESS_QUERY_INFORMATION | syscall.SYNCHRONIZE
 
 	logutil.Trace("cmds %v", cmds)
-	if len(cmds) > 0 && !filepath.IsAbs(cmds[0]) {
-		var pathenv string
-		pathenv = os.Getenv("PATH")
-		if len(pathenv) > 0 {
-			var nsarr []string
-			var curdir string
-			nsarr = strings.Split(pathenv, ";")
-			for _, curdir = range nsarr {
-				var curexe string
-				curexe = fmt.Sprintf("%s\\%s", curdir, cmds[0])
-				if _exist_file(curexe) {
-					cmds[0] = curexe
-					break
-				}
-			}
-		}
-	}
-
+	cmds = _expand_cmds(cmds)
 	cmd = &exec.Cmd{}
 	cmd.Path = cmds[0]
 	cmd.Args = cmds
