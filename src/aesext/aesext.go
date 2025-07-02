41ecb52cdab509be097217fbdb112785e77d982b
jeppeter <jeppeter@gmail.com>
2025-07-02 13:24:49 +0800

    to add aesext

---
+++ src/aesext/aesext.go
@@ -0,0 +1,47 @@
+package aesext
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"dbgutil"
+)
+
+func AesEncEcb(plaintext []byte, key []byte) (ciphertext []byte, err error) {
+	var cblock cipher.Block
+	var totaln int
+	cblock, err = aes.NewCipher(key)
+	if err != nil {
+		err = dbgutil.FormatError("key cipher error %s", err.Error())
+		return
+	}
+	if len(plaintext)%cblock.BlockSize() != 0 {
+		err = dbgutil.FormatError("plaintext len %d %% %d != 0", len(plaintext), cblock.BlockSize())
+		return
+	}
+	ciphertext = make([]byte, len(plaintext))
+	for totaln = 0; totaln < len(plaintext); totaln += cblock.BlockSize() {
+		cblock.Encrypt(ciphertext[totaln:], plaintext[totaln:])
+	}
+	err = nil
+	return
+}
+
+func AesDecEcb(ciphertext []byte, key []byte) (plaintext []byte, err error) {
+	var cblock cipher.Block
+	var totaln int
+	cblock, err = aes.NewCipher(key)
+	if err != nil {
+		err = dbgutil.FormatError("key cipher error %s", err.Error())
+		return
+	}
+	if len(ciphertext)%cblock.BlockSize() != 0 {
+		err = dbgutil.FormatError("ciphertext len %d %% %d != 0", len(ciphertext), cblock.BlockSize())
+		return
+	}
+	plaintext = make([]byte, len(ciphertext))
+	for totaln = 0; totaln < len(ciphertext); totaln += cblock.BlockSize() {
+		cblock.Decrypt(plaintext[totaln:], ciphertext[totaln:])
+	}
+	err = nil
+	return
+}
