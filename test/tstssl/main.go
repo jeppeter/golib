97f7613d11cc9b2bb1f834ef6b7a5fa75757099e
jeppeter <jeppeter@gmail.com>
2025-07-04 13:12:12 +0800

    to make rsavfy and rsasign ok

--- test/tstssl/main.go
+++ test/tstssl/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bytes"
+	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"dbgutil"
@@ -18,10 +20,30 @@ import (
 	"net"
 	"net/url"
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
 	"time"
 )
+
+func read_pem_or_der(infile string) (outb []byte, err error) {
+	var inb []byte
+	var block *pem.Block
+	var leftbytes []byte
+	inb, err = fileop.ReadFileBytes(infile)
+	if err != nil {
+		return
+	}
+	block, leftbytes = pem.Decode(inb)
+	if len(leftbytes) > 0 {
+		logutil.DebugBuffer(leftbytes, "leftbytes in [%s]", infile)
+		outb = inb
+		err = nil
+		return
+	}
+	outb = block.Bytes
+	return
+}
 
 func Genkeycert_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx interface{}) (err error) {
 	var sarr []string
@@ -230,10 +252,171 @@ func Dertopem_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx int
 	return
 }
 
+func rsa_sign_sha256(keyfile string, indata []byte) (signdata []byte, err error) {
+	var keydata []byte
+	var hashed []byte
+	var pkany any
+	var rsakey *rsa.PrivateKey
+	keydata, err = read_pem_or_der(keyfile)
+	if err != nil {
+		return
+	}
+
+	pkany, err = x509.ParsePKCS8PrivateKey(keydata)
+	if err != nil {
+		err = dbgutil.FormatError("[%s] not valid rsa %s", keyfile, err.Error())
+		return
+	}
+	switch pkany.(type) {
+	case *rsa.PrivateKey:
+		rsakey = pkany.(*rsa.PrivateKey)
+	default:
+		err = dbgutil.FormatError("key is not rsakey type [%s]", reflect.TypeOf(pkany))
+		return
+	}
+
+	hasher := sha256.New()
+	hasher.Write(indata)
+
+	hashed = hasher.Sum(nil)
+	signdata, err = rsa.SignPKCS1v15(nil, rsakey, crypto.SHA256, hashed)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func Rsasign_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx interface{}) (err error) {
+	var input string
+	var sarr []string
+	var keyfile string
+	var signfile string
+	var signdata []byte
+	var indata []byte
+	err = nil
+	if ns == nil {
+		return nil
+	}
+	err = logutil.InitLog(ns)
+	if err != nil {
+		logutil.Error("can not Initlog err[%s]", err.Error())
+		return err
+	}
+
+	sarr = ns.GetArray("subnargs")
+	if len(sarr) < 1 {
+		err = dbgutil.FormatError("need TAG")
+		return
+	}
+
+	keyfile = sarr[0]
+	input = sarr[1]
+	signfile = sarr[2]
+	indata, err = fileop.ReadFileBytes(input)
+	if err != nil {
+		return
+	}
+
+	signdata, err = rsa_sign_sha256(keyfile, indata)
+	if err != nil {
+		return
+	}
+
+	_, err = fileop.WriteFileBytes(signfile, signdata)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func rsa_verify_sha256(certfile string, indata []byte, signdata []byte) (err error) {
+	var certdata, hashed []byte
+	var pubkey *rsa.PublicKey
+	var cert *x509.Certificate
+	certdata, err = read_pem_or_der(certfile)
+	if err != nil {
+		return
+	}
+
+	cert, err = x509.ParseCertificate(certdata)
+	if err != nil {
+		return
+	}
+
+	switch cert.PublicKey.(type) {
+	case *rsa.PublicKey:
+		pubkey = cert.PublicKey.(*rsa.PublicKey)
+	default:
+		err = dbgutil.FormatError("key is not rsakey type [%s]", reflect.TypeOf(cert.PublicKey))
+		return
+	}
+
+	hasher := sha256.New()
+	hasher.Write(indata)
+
+	hashed = hasher.Sum(nil)
+
+	err = rsa.VerifyPKCS1v15(pubkey, crypto.SHA256, hashed, signdata)
+	if err != nil {
+		err = dbgutil.FormatError("verify error %s", err.Error())
+		return
+	}
+	return
+}
+
+func Rsavfy_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx interface{}) (err error) {
+	var input string
+	var indata, signdata []byte
+	var sarr []string
+	var certfile string
+	var signfile string
+
+	err = nil
+	if ns == nil {
+		return nil
+	}
+	err = logutil.InitLog(ns)
+	if err != nil {
+		logutil.Error("can not Initlog err[%s]", err.Error())
+		return err
+	}
+
+	sarr = ns.GetArray("subnargs")
+	if len(sarr) < 1 {
+		err = dbgutil.FormatError("need TAG")
+		return
+	}
+
+	certfile = sarr[0]
+	input = sarr[1]
+	signfile = sarr[2]
+
+	indata, err = fileop.ReadFileBytes(input)
+	if err != nil {
+		return
+	}
+
+	signdata, err = fileop.ReadFileBytes(signfile)
+	if err != nil {
+		return
+	}
+
+	err = rsa_verify_sha256(certfile, indata, signdata)
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("verify [%s] [%s] [%s] succ\n", certfile, input, signfile)
+	return
+}
+
 func init() {
 	Genkeycert_handler(nil, nil, nil)
 	Pemtoder_handler(nil, nil, nil)
 	Dertopem_handler(nil, nil, nil)
+	Rsasign_handler(nil, nil, nil)
+	Rsavfy_handler(nil, nil, nil)
 }
 func main() {
 	var commandline string
@@ -254,6 +437,12 @@ func main() {
 		},
 		"dertopem<Dertopem_handler>##TAG from input to output to writeout##" : {
 			"$" : 1
+		},
+		"rsasign<Rsasign_handler>##privkeyfile inputfile signfile to sign data##" : {
+			"$": 3
+		},
+		"rsavfy<Rsavfy_handler>##certfile inputfile signfile to verify data##" : {
+			"$" : 3
 		}
 
 	}`
