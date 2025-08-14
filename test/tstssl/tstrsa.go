9dac850d2ab2025281893aed0fd127bab11f9ab9
jeppeter <jeppeter@gmail.com>
2025-08-14 09:52:14 +0800

    to make rsapsssign rsapssvfy ok

--- test/tstssl/tstrsa.go
+++ test/tstssl/tstrsa.go
@@ -101,6 +101,99 @@ func Rsasign_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx inte
 	return
 }
 
+func rsa_pss_sign_sha256(keydata []byte, indata []byte, psslen int) (signdata []byte, err error) {
+	var hashed []byte
+	var pkany any
+	var rsakey *rsa.PrivateKey
+	var pssopt *rsa.PSSOptions
+
+	pkany, err = x509.ParsePKCS8PrivateKey(keydata)
+	if err != nil {
+		err = dbgutil.FormatError("keydata not valid rsa %s", err.Error())
+		return
+	}
+	switch pkany.(type) {
+	case *rsa.PrivateKey:
+		rsakey = pkany.(*rsa.PrivateKey)
+	default:
+		err = dbgutil.FormatError("key is not rsakey type [%s]", reflect.TypeOf(pkany))
+		return
+	}
+
+	hasher := sha256.New()
+	hasher.Write(indata)
+
+	hashed = hasher.Sum(nil)
+
+	pssopt = &rsa.PSSOptions{}
+	pssopt.SaltLength = psslen
+	pssopt.Hash = crypto.SHA256
+
+	signdata, err = rsa.SignPSS(rand.Reader, rsakey, crypto.SHA256, hashed, pssopt)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func Rsapsssign_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx interface{}) (err error) {
+	var input string
+	var sarr []string
+	var keyfile string
+	var signfile string
+	var signdata []byte
+	var indata []byte
+	var keydata []byte
+	var psslen int
+	err = nil
+	if ns == nil {
+		return nil
+	}
+	err = logutil.InitLog(ns)
+	if err != nil {
+		logutil.Error("can not Initlog err[%s]", err.Error())
+		return err
+	}
+
+	sarr = ns.GetArray("subnargs")
+	if len(sarr) < 3 {
+		err = dbgutil.FormatError("need keyfile inputfile signfile")
+		return
+	}
+
+	psslen = ns.GetInt("psslength")
+
+	keyfile = sarr[0]
+	input = sarr[1]
+	signfile = sarr[2]
+
+	keydata, err = read_pem_or_der(keyfile)
+	if err != nil {
+		return
+	}
+
+	if err != nil {
+		return
+	}
+
+	indata, err = fileop.ReadFileBytes(input)
+	if err != nil {
+		return
+	}
+
+	signdata, err = rsa_pss_sign_sha256(keydata, indata, psslen)
+	if err != nil {
+		return
+	}
+
+	_, err = fileop.WriteFileBytes(signfile, signdata)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 func rsa_verify_sha256(keydata []byte, indata []byte, signdata []byte) (err error) {
 	var hashed []byte
 	var pubkey *rsa.PublicKey
@@ -139,9 +232,9 @@ func Rsavfy_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx inter
 	var input string
 	var indata, signdata []byte
 	var sarr []string
-	var certfile string
+	var keyfile string
 	var signfile string
-	var certdata []byte
+	var keydata []byte
 
 	err = nil
 	if ns == nil {
@@ -159,11 +252,11 @@ func Rsavfy_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx inter
 		return
 	}
 
-	certfile = sarr[0]
+	keyfile = sarr[0]
 	input = sarr[1]
 	signfile = sarr[2]
 
-	certdata, err = read_pem_or_der(certfile)
+	keydata, err = read_pem_or_der(keyfile)
 	if err != nil {
 		return
 	}
@@ -178,12 +271,105 @@ func Rsavfy_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx inter
 		return
 	}
 
-	err = rsa_verify_sha256(certdata, indata, signdata)
+	err = rsa_verify_sha256(keydata, indata, signdata)
 	if err != nil {
 		return
 	}
 
-	fmt.Printf("verify [%s] [%s] [%s] succ\n", certfile, input, signfile)
+	fmt.Printf("verify [%s] [%s] [%s] succ\n", keyfile, input, signfile)
+	return
+}
+
+func rsa_pss_verify_sha256(keydata []byte, indata []byte, signdata []byte, psslen int) (err error) {
+	var hashed []byte
+	var pubkey *rsa.PublicKey
+	var pkany any
+	var rsakey *rsa.PrivateKey
+	var pssopt *rsa.PSSOptions
+
+	pkany, err = x509.ParsePKCS8PrivateKey(keydata)
+	if err != nil {
+		err = dbgutil.FormatError("keydata not valid rsa %s", err.Error())
+		return
+	}
+	switch pkany.(type) {
+	case *rsa.PrivateKey:
+		rsakey = pkany.(*rsa.PrivateKey)
+	default:
+		err = dbgutil.FormatError("key is not rsakey type [%s]", reflect.TypeOf(pkany))
+		return
+	}
+
+	pubkey = &rsakey.PublicKey
+
+	hasher := sha256.New()
+	hasher.Write(indata)
+
+	hashed = hasher.Sum(nil)
+
+	pssopt = &rsa.PSSOptions{}
+	pssopt.SaltLength = psslen
+	pssopt.Hash = crypto.SHA256
+
+	err = rsa.VerifyPSS(pubkey, crypto.SHA256, hashed, signdata, pssopt)
+	if err != nil {
+		err = dbgutil.FormatError("verify error %s", err.Error())
+		return
+	}
+	return
+}
+
+func Rsapssvfy_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx interface{}) (err error) {
+	var input string
+	var indata, signdata []byte
+	var sarr []string
+	var keyfile string
+	var signfile string
+	var keydata []byte
+	var psslen int
+
+	err = nil
+	if ns == nil {
+		return nil
+	}
+	err = logutil.InitLog(ns)
+	if err != nil {
+		logutil.Error("can not Initlog err[%s]", err.Error())
+		return err
+	}
+
+	sarr = ns.GetArray("subnargs")
+	if len(sarr) < 1 {
+		err = dbgutil.FormatError("need TAG")
+		return
+	}
+	psslen = ns.GetInt("psslength")
+
+	keyfile = sarr[0]
+	input = sarr[1]
+	signfile = sarr[2]
+
+	keydata, err = read_pem_or_der(keyfile)
+	if err != nil {
+		return
+	}
+
+	indata, err = fileop.ReadFileBytes(input)
+	if err != nil {
+		return
+	}
+
+	signdata, err = fileop.ReadFileBytes(signfile)
+	if err != nil {
+		return
+	}
+
+	err = rsa_pss_verify_sha256(keydata, indata, signdata, psslen)
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("verify [%s] [%s] [%s] psslen %d succ\n", keyfile, input, signfile, psslen)
 	return
 }
 
@@ -248,19 +434,28 @@ func Rsagen_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx inter
 func init() {
 	Rsasign_handler(nil, nil, nil)
 	Rsavfy_handler(nil, nil, nil)
+	Rsapsssign_handler(nil, nil, nil)
+	Rsapssvfy_handler(nil, nil, nil)
 	Rsagen_handler(nil, nil, nil)
 }
 
 func load_rsa_command(parser *extargsparse.ExtArgsParse) (err error) {
 
 	var commandline string = `{
+		"psslength##pss length set 0 for auto -1 for equal hash##" : 0,
 		"rsagen<Rsagen_handler>##bits to generate rsa key file##" : {
 			"$" : 1
 		},
 		"rsasign<Rsasign_handler>##privkeyfile inputfile signfile to sign data##" : {
 			"$": 3
 		},
-		"rsavfy<Rsavfy_handler>##certfile inputfile signfile to verify data##" : {
+		"rsavfy<Rsavfy_handler>##privkeyfile inputfile signfile to verify data##" : {
+			"$" : 3
+		},
+		"rsapsssign<Rsapsssign_handler>##privkeyfile inputdata signfile to sign data##" : {
+			"$" : 3
+		},
+		"rsapssvfy<Rsapssvfy_handler>##privkeyfile inputfile signfile to verify data##" : {
 			"$" : 3
 		}
 
