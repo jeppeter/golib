4401afb231dd43b3dd233d7e9054a9f811a7c1a1
jeppeter <jeppeter@gmail.com>
2026-04-01 16:47:12 +0800

    to pass dlfunc in windows environment

--- src/dlfunc/dlfunc_linux.go
+++ src/dlfunc/dlfunc_linux.go
@@ -1,4 +1,4 @@
-package dlopen
+package dlfunc
 
 
 // 
--- src/dlfunc/dlfunc_windows.go
+++ src/dlfunc/dlfunc_windows.go
@@ -1,7 +1,9 @@
-package dlopen
+package dlfunc
 
 import (
+	"fmt"
 	"syscall"
+	"unsafe"
 )
 
 type DllLib struct {
@@ -51,16 +53,16 @@ func (ptr *DllLib) GetFunc(procname string) (retptr *DllFunc, err error) {
 		return
 	}
 	retptr = &DllFunc{}
-	retptr.funcptr = unsafe.Pointer(funcptr)
+	retptr.funcptr = unsafe.Pointer(funcaddr)
 	retptr.libptr = ptr
 	retptr.procname = procname
 	err = nil
 	return
 }
 
-func make_input_var(num int, a ...interface{}) (retvar []uintptr) {
+func make_input_var(num int, a ...uintptr) (retvar []uintptr) {
 	retvar = []uintptr{}
-	for _, ai = range a {
+	for _, ai := range a {
 		retvar = append(retvar, uintptr(ai))
 	}
 
@@ -70,29 +72,29 @@ func make_input_var(num int, a ...interface{}) (retvar []uintptr) {
 	return
 }
 
-func (fptr *DllFunc) CallN(num int, a ...interface{}) (retval uintptr, err error) {
+func (fptr *DllFunc) CallN(num int, a ...uintptr) (retval uintptr, err error) {
 
 	var errv syscall.Errno
 	var inputvar []uintptr
 
 	if num <= 3 {
 		inputvar = make_input_var(3, a...)
-		retval, _, errv = syscall.Syscall(uintptr(fptr.funcptr), num, inputvar[0], inputvar[1], inputvar[2])
+		retval, _, errv = syscall.Syscall(uintptr(fptr.funcptr), uintptr(num), inputvar[0], inputvar[1], inputvar[2])
 	} else if num <= 6 {
 		inputvar = make_input_var(6, a...)
-		retval, _, errv = syscall.Syscall6(uintptr(fptr.funcptr), num, inputvar[0], inputvar[1], inputvar[2], inputvar[3], inputvar[4], inputvar[5])
+		retval, _, errv = syscall.Syscall6(uintptr(fptr.funcptr), uintptr(num), inputvar[0], inputvar[1], inputvar[2], inputvar[3], inputvar[4], inputvar[5])
 	} else if num <= 9 {
 		inputvar = make_input_var(9, a...)
-		retval, _, errv = syscall.Syscall9(uintptr(fptr.funcptr), num, inputvar[0], inputvar[1], inputvar[2], inputvar[3], inputvar[4], inputvar[5], inputvar[6], inputvar[7], inputvar[8])
+		retval, _, errv = syscall.Syscall9(uintptr(fptr.funcptr), uintptr(num), inputvar[0], inputvar[1], inputvar[2], inputvar[3], inputvar[4], inputvar[5], inputvar[6], inputvar[7], inputvar[8])
 	} else if num <= 12 {
 		inputvar = make_input_var(12, a...)
-		retval, _, errv = syscall.Syscall12(uintptr(fptr.funcptr), num, inputvar[0], inputvar[1], inputvar[2], inputvar[3], inputvar[4], inputvar[5], inputvar[6], inputvar[7], inputvar[8], inputvar[9], inputvar[10], inputvar[11])
+		retval, _, errv = syscall.Syscall12(uintptr(fptr.funcptr), uintptr(num), inputvar[0], inputvar[1], inputvar[2], inputvar[3], inputvar[4], inputvar[5], inputvar[6], inputvar[7], inputvar[8], inputvar[9], inputvar[10], inputvar[11])
 	} else if num <= 15 {
 		inputvar = make_input_var(15, a...)
-		retval, _, errv = syscall.Syscall15(uintptr(fptr.funcptr), num, inputvar[0], inputvar[1], inputvar[2], inputvar[3], inputvar[4], inputvar[5], inputvar[6], inputvar[7], inputvar[8], inputvar[9], inputvar[10], inputvar[11], inputvar[12], inputvar[13], inputvar[14])
+		retval, _, errv = syscall.Syscall15(uintptr(fptr.funcptr), uintptr(num), inputvar[0], inputvar[1], inputvar[2], inputvar[3], inputvar[4], inputvar[5], inputvar[6], inputvar[7], inputvar[8], inputvar[9], inputvar[10], inputvar[11], inputvar[12], inputvar[13], inputvar[14])
 	} else if num <= 18 {
 		inputvar = make_input_var(18, a...)
-		retval, _, errv = syscall.Syscall18(uintptr(fptr.funcptr), num, inputvar[0], inputvar[1], inputvar[2], inputvar[3], inputvar[4], inputvar[5], inputvar[6], inputvar[7], inputvar[8], inputvar[9], inputvar[10], inputvar[11], inputvar[12], inputvar[13], inputvar[14], inputvar[15], inputvar[16], inputvar[17])
+		retval, _, errv = syscall.Syscall18(uintptr(fptr.funcptr), uintptr(num), inputvar[0], inputvar[1], inputvar[2], inputvar[3], inputvar[4], inputvar[5], inputvar[6], inputvar[7], inputvar[8], inputvar[9], inputvar[10], inputvar[11], inputvar[12], inputvar[13], inputvar[14], inputvar[15], inputvar[16], inputvar[17])
 	} else {
 		err = fmt.Errorf("num %d > 18", num)
 		return
