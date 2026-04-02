2855b664b7a37c114cbbb38d85c5cded6afd72c8
jeppeter <jeppeter@gmail.com>
2026-04-02 17:25:46 +0800

    to add utils.go

---
+++ src/dlfunc/go.mod
@@ -0,0 +1,3 @@
+module dlfunc
+
+go 1.22.0
---
+++ src/dlfunc/utils.go
@@ -0,0 +1,32 @@
+package dlfunc
+
+import (
+	"unsafe"
+)
+
+// MakeCString converts the given string to a null terminated C byte-slice/char array
+func MakeCString(s string) []byte {
+	return append([]byte(s), 0)
+}
+
+// MakeGoStringFromPointer converts the given pointer to a null terminated C string to a go string
+func MakeGoStringFromPointer(s uintptr) string {
+	if s == 0 {
+		return ""
+	}
+	bval := (*[1 << 30]byte)(unsafe.Pointer(s))
+	return MakeGoStringFromSlice(bval[:])
+}
+
+// MakeGoStringFromSlice converts the given byte slice containing a null terminated C string to a go string
+func MakeGoStringFromSlice(s []byte) string {
+	if len(s) == 0 {
+		return ""
+	}
+	for i := range s {
+		if s[i] == 0 {
+			return string(s[:i])
+		}
+	}
+	return string(s[:])
+}
