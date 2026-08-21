6d80c8a3fac310a2ad9d52fa55b11dd6b21e0077
jeppeter <jeppeter@gmail.com>
2026-08-21 21:39:48 +0800

    to make aesext test ok

--- src/aesext/aesext.go
+++ src/aesext/aesext.go
@@ -26,6 +26,30 @@ func AesEncEcb(plaintext []byte, key []byte) (ciphertext []byte, err error) {
 	return
 }
 
+func unpad_bytes(inbytes []byte,blksize int) (retbytes []byte,err error) {
+	var leftsize int
+	var idx int
+	if (len(inbytes) % blksize ) != 0 {
+		err = dbgutil.FormatError("input size [%d] %% %d != 0",len(inbytes),blksize)
+		return
+	}
+
+	leftsize = int(inbytes[len(inbytes)-1])
+	retbytes = make([]byte,len(inbytes) - leftsize)
+	for idx = 0 ;idx<(len(inbytes) - leftsize) ; idx += 1 {
+		retbytes[idx] = inbytes[idx]
+	}
+
+	for idx = len(retbytes);idx < len(inbytes) ; idx += 1 {
+		if inbytes[idx] != byte(leftsize) {
+			err = dbgutil.FormatError("[%d] [0x%02x] != 0x%02x", idx,inbytes[idx],leftsize)
+			return
+		}
+	}
+	err = nil
+	return
+}
+
 func AesDecEcb(ciphertext []byte, key []byte) (plaintext []byte, err error) {
 	var cblock cipher.Block
 	var totaln int
@@ -47,25 +71,50 @@ func AesDecEcb(ciphertext []byte, key []byte) (plaintext []byte, err error) {
 }
 
 func AesDecCbc(ciphertext []byte, key []byte, iv []byte) (plaintext []byte, err error) {
+	var obytes []byte
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return
 	}
 	dec := cipher.NewCBCDecrypter(block, iv)
-	plaintext = make([]byte, len(ciphertext))
-	dec.CryptBlocks(plaintext, ciphertext)
-	err = nil
+	obytes = make([]byte, len(ciphertext))
+	dec.CryptBlocks(obytes, ciphertext)
+	plaintext,err = unpad_bytes(obytes,aes.BlockSize)
 	return
 }
 
-func AesEncCbc(plaintext []byte, key []byte, iv []byte, blksize int) (ciphertext []byte, err error) {
+func pad_bytes(inbytes []byte,blksize int) (retbytes []byte) {
+	var retlen int = len(inbytes)
+	var i int
+	retlen += (blksize - 1)
+	retlen /= blksize
+	retlen *= blksize
+	if retlen == len(inbytes) {
+		retlen += blksize
+	}
+
+	var padbyte byte
+	padbyte = byte(retlen - len(inbytes))
+
+	retbytes = make([]byte,retlen)
+	for i =0 ; i < len(inbytes) ; i+= 1 {
+		retbytes[i] = inbytes[i]
+	}
+	for i = len(inbytes);i < retlen;i+= 1 {
+		retbytes[i] = padbyte
+	}
+	return 
+}
+
+func AesEncCbc(plaintext []byte, key []byte, iv []byte) (ciphertext []byte, err error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
+		err = dbgutil.FormatError("NewCipher for key error %s",err.Error())
 		return
 	}
 
 	enc := cipher.NewCBCEncrypter(block, iv)
-	nplain := Pad(plaintext, aes.BlockSize)
+	nplain := pad_bytes(plaintext, aes.BlockSize)
 	ciphertext = make([]byte, len(nplain))
 	enc.CryptBlocks(ciphertext, nplain)
 	err = nil
---
+++ test/tstcrypt/main.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"aesext"
+	"dbgutil"
+	"fmt"
+	"fileop"
+	"github.com/jeppeter/go-extargsparse"
+	"github.com/tebeka/atexit"
+	"logutil"
+	"os"
+)
+
+func Aescbcenc_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx interface{}) (err error) {
+	var sarr []string
+	var key []byte
+	var iv []byte
+	var data []byte
+	var encbytes []byte
+	var ofile string
+	err = nil
+	if ns == nil {
+		return nil
+	}
+	err = logutil.InitLog(ns)
+	if err != nil {
+		logutil.Error("can not Initlog err[%s]", err.Error())
+		return err
+	}
+
+	sarr = ns.GetArray("subnargs")
+	if len(sarr) < 3 {
+		err = dbgutil.FormatError("need aesfile ivfile datafile")
+		return
+	}
+
+	ofile = ns.GetString("output")
+
+	key, err = fileop.ReadFileBytes(sarr[0])
+	if err != nil {
+		return
+	}
+
+	iv, err = fileop.ReadFileBytes(sarr[1])
+	if err != nil {
+		return
+	}
+
+
+	data, err = fileop.ReadFileBytes(sarr[2])
+	if err != nil {
+		return
+	}
+
+	encbytes, err = aesext.AesEncCbc(data,key,iv)
+	if err != nil {
+		return
+	}
+
+	_, err = fileop.WriteFileBytes(ofile,encbytes)
+	if err != nil {
+		return
+	}
+
+	err = nil
+	return
+}
+
+func Aescbcdec_handler(ns *extargsparse.NameSpaceEx, ostruct interface{}, ctx interface{}) (err error) {
+	var sarr []string
+	var key []byte
+	var iv []byte
+	var data []byte
+	var ofile string
+	var plaintext []byte
+	err = nil
+	if ns == nil {
+		return nil
+	}
+	err = logutil.InitLog(ns)
+	if err != nil {
+		logutil.Error("can not Initlog err[%s]", err.Error())
+		return err
+	}
+
+	sarr = ns.GetArray("subnargs")
+	if len(sarr) < 3 {
+		err = dbgutil.FormatError("need aesfile ivfile datafile")
+		return
+	}
+
+	ofile = ns.GetString("output")
+
+	key, err = fileop.ReadFileBytes(sarr[0])
+	if err != nil {
+		return
+	}
+
+	iv, err = fileop.ReadFileBytes(sarr[1])
+	if err != nil {
+		return
+	}
+
+
+	data, err = fileop.ReadFileBytes(sarr[2])
+	if err != nil {
+		return
+	}
+
+	plaintext, err = aesext.AesDecCbc(data,key,iv)
+	if err != nil {
+		return
+	}
+
+	_, err = fileop.WriteFileBytes(ofile,plaintext)
+	if err != nil {
+		return
+	}
+
+	err = nil
+	return
+}
+
+func init() {
+	Aescbcenc_handler(nil, nil, nil)
+	Aescbcdec_handler(nil, nil, nil)
+}
+func main() {
+	var commandline string
+	var err error
+	var parser *extargsparse.ExtArgsParse
+	var ns *extargsparse.NameSpaceEx
+
+	commandline = `{
+		"input|i" : null,
+		"output|o" : null,
+		"aescbcenc<Aescbcenc_handler>##aeskey ivfile datafile with output to output option##" : {
+			"$" : 3
+		},
+		"aescbcdec<Aescbcdec_handler>##aeskey ivfile datafile with output to output option##" : {
+			"$" : 3
+		}
+
+	}`
+
+	parser, err = extargsparse.NewExtArgsParse(nil, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "can not make parser err[%s]\n", err.Error())
+		atexit.Exit(5)
+	}
+
+	err = logutil.PrepareLog(parser)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "can not set [%s]\n", err.Error())
+		atexit.Exit(5)
+	}
+	err = parser.LoadCommandLineString(commandline)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "can not parse %s\n", commandline)
+		atexit.Exit(5)
+	}
+
+
+	ns, err = parser.ParseCommandLineEx(nil, nil, nil, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "can not use parse command line [%s]\n", err.Error())
+		atexit.Exit(4)
+	}
+	if len(ns.GetString("subcommand")) == 0 {
+		fmt.Fprintf(os.Stderr, "can not get subcommand\n")
+		atexit.Exit(5)
+	}
+	//fmt.Fprintf(os.Stdout, "subcommand [%s] succ\n", ns.GetString("subcommand"))
+	atexit.Exit(0)
+	return
+}
